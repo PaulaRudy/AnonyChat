d6139b3585b8e86d441d5d7c139215da71a5d597
PaulaRudy <paularudy@gmail.com>
2014-02-22 13:49:24 -0500

    Fixed RoutingTable.h and RoutingTable.cpp.

--- AnonyChatLib/include/Routing/RoutingTable.h
+++ AnonyChatLib/include/Routing/RoutingTable.h
@@ -2,35 +2,37 @@
 #define ROUTINGTABLE_H_
 
 #include <iostream>
-#include <list>
+#include <map>
+#include <vector>
+#include <algorithm>
 #include "message.h"
 #include "Connection.h"
 
 using namespace std;
 
-class VirtualAddress {
+/**
+ * An struct made of an unsigned char array representing the
+ * virtual address of a destination entry in the Routing Table
+ */
+struct VirtualAddress {
 	unsigned char virtual_address[1024];
+
 };
 
-map<VirtualAddress, map<string, int>> RoutingTable;
+class RoutingTable {
+private:
+	map<VirtualAddress, map<string, int>> table; //The actual routing table
+	map<VirtualAddress, map<string, int>>::iterator it;//An iterator to traverse the table by destination virtual address
+	map<string, int>::iterator listIT;//An iterator to traverse the list of utility count/connection IP address pairs for a single destination virtual address entry in the routing table
+public:
+	RoutingTable();//Constructor. See RoutingTable.cpp for more details.
+	~RoutingTable();//Destructor. See RoutingTable.cpp for more details.
+	int UpdateTableEntry(VirtualAddress, string, int);//Function to add or update an entry in the routing table. See RoutingTable.cpp for more details.
+	int RemoveTableEntry(VirtualAddress);//Function to remove an entry from the routing table. See RoutingTable.cpp for more details.]
+	bool Routing_SortConnections(pair<string, int> , pair<string, int> );// Function used to sort the map of connection information for a given destination virtual address entry in the routing table. See RoutingTable.cpp for more details.
+};
 
-// function to add an entry to the routing table
-// takes as an unsigned char array representing the virtual address for which the entry is to be added
-// the ip address of the neighbor the routing table entry is to represent, and an int
-// representing the number of utility counters when the message was received
-// returns an int indicating success or failure
-int UpdateTableEntry (VirtualAddress, string, int);
 
-// function to remove an entry from the routing table
-// takes as an argument an unsigned character array indicating the virtual address
-// to remove information for from the routing table
-int RemoveTableEntry (VirtualAddress);
-
-// function used to sort the map of connection information for a given virtual address
-// takes as an argument the information for two connections
-// and returns true if they are in the correct order
-// otherwise returns false
-bool Routing_SortConnections(pair<string, int>, pair<string, int>);
 
 // function to determine the path of a message
 // takes as argument the message to be passed
@@ -39,6 +41,6 @@ bool Routing_SortConnections(pair<string, int>, pair<string, int>);
 // returns an int indicating success or failure
 //		failure is possible if every neighbor is down!
 //		otherwise the routing protocol will attempt to recover
-int RouteMessage (Message, Connection);
+int RouteMessage(Message, Connection);//TODO: finish this.
 
 #endif
--- AnonyChatLib/src/Routing/RoutingTable.cpp
+++ AnonyChatLib/src/Routing/RoutingTable.cpp
@@ -1,97 +1,117 @@
-#include <iostream>
-#include <vector>
-
 #include "RoutingTable.h"
 
-using namespace std;
-
-int UpdateTableEntry (VirtualAddress destaddr, string ipaddr, int utilcounters) {
-	map<VirtualAddress, map<string, int>>::iterator it;
-	map<string, int> interfacelist;
-	map<string, int>::iterator listIT;
-	vector<pair<string, int>> mapSort;
-
-	// find the entry associated with the virtual address
-	it = RoutingTable.find(destaddr);
-
-	// if there was no entry in the routing table associated
-	// with the specified destination address
-	if (it == RoutingTable.end()) {
-
-		// first we need to make our connection info into a map
-		interfacelist.insert(pair<string, int> (ipaddr, utilcounters));
-
-		// now we just insert it into the routing table map
-		RoutingTable.insert(pair<VirtualAddress, map<string, int>> (destaddr, interfacelist));
-
-		// and we return 0! this was the easy one...
-		return 0;
-
-	} // otherwise if there WAS an entry in the table associated with this virtual address
-
-	// next we check if this virtual address is already associated with this connection ip
-	listIT = it->second.find(ipaddr);
-
-	// if there was not already an association between this connection ip and virtual address
-	if (listIT == it->second.end()) {
-
-		// then we just need to insert this connection/util counter combination
-		it->second.insert(pair<string, int> (ipaddr, utilcounters));
-
-		// in order to sort the interior map we need to convert it to a vector
-		mapSort = new vector<pair<string, int>> (it->second.begin(), it->second.end());
-
-		// we now can sort the map using library functions
-		sort(mapSort.begin(), mapSort.end(), &Routing_SortConnections);
-
-		// we should be able to return now
-		return 0;
-
-	// if there was an association between this connection and virtual ip address
-	// and the associated utility counters are more than the new value
-	} else if (listIT->second > utilcounters) {
-
-		// then we replace the old utility counters value
-		listIT->second = utilcounters;
-
-		// and we sort the map again
-		// then we just need to insert this connection/util counter combination
-		it->second.insert(pair<string, int> (ipaddr, utilcounters));
-
-		// in order to sort the interior map we need to convert it to a vector
-		mapSort = new vector<pair<string, int>> (it->second.begin(), it->second.end());
-
-		// we now can sort the map using library functions
-		sort(mapSort.begin(), mapSort.end(), &Routing_SortConnections);
-
-		// then we can return
-		return 0;
-
-	} // otherwise we don't need to do anything (the new route was not fewer hops)
-
-	return 0;
+RoutingTable::RoutingTable(void) {
+//TODO: Put code here to initalize the routing table from a file
 }
 
+/**
+ * Function to add an entry to the routing table
+ * Takes:
+ * 		"destaddr"
+ * 			A VirtualAddress (see RoutingTable.h)representing the destination virtual address
+ * 			for which the entry is to be updated or added
+ * 		"ipaddr"
+ * 			The ip address of the neighbor the routing table entry is to represent
+ * 		"utilcounter"
+ * 			An int representing the utility counter of the message when it was received
+ *
+ * Returns an int indicating success (0) or failure (1)
+ */
+int RoutingTable::UpdateTableEntry(VirtualAddress destaddr, string ipaddr,
+		int utilcounter) {
 
-int RemoveTableEntry (VirtualAddress rmAddr) {
-	map<VirtualAddress, map<string, int>>::iterator it;
+	it = table.find(destaddr);// find the entry associated with the destination virtual address
 
-	// we have to find the entry to remove
-	it = RoutingTable.find(rmAddr);
+	if (it == table.end()) {// If there was no entry in the routing table associated with the specified destination address....
 
-	// if we didn't find anything return an error
-	if (it == RoutingTable.end()) return 1;
+		//Make a new entry and add it to the table:
+		map<string, int> interfacelist;
 
-	// otherwise we just remove it
-	RoutingTable.erase(it);
+		interfacelist.insert(pair<string, int> (ipaddr, utilcounter));//First we need to make our connection info (IP address of the connection and the associated utility count) into a map
 
-	return 0;
+		table.insert(
+				pair<VirtualAddress, map<string, int>> (destaddr, interfacelist));// Now we just insert it into the routing table map associated with the destination virtual address
+
+	} else { //We found an entry associated with this destination virtual address in the table
+
+		listIT = it->second.find(ipaddr); //Check the entry for this destination virtual address's map of utility count to connection IP address for an existing association with this connection (identified by ip address)
+
+		if (listIT == it->second.end()) {// If there *isn't* an existing association between this connection ip and this destination virtual address in the map
+
+			it->second.insert(pair<string, int> (ipaddr, utilcounter));// Then we need to insert this connection/util counter combination into the map for this VID
+
+			//Sort the map (the map of utility count/connection IP address pairs for this connection) before returning:
+
+			vector<pair<string, int>> mapSort;
+
+			mapSort = new vector<pair<string, int>> (it->second.begin(), //In order to sort the map we need to convert it to a vector first
+					it->second.end());
+
+			sort(mapSort.begin(), mapSort.end(), &RoutingTable::Routing_SortConnections);//We can now sort the map using "Routing_SortConnections" function (defined in this file)
+
+		} else if (listIT->second > utilcounter) {// If there *was* an association between this connection and this destination virtual ip address, and the associated utility counter is more than the new utility count
+
+			listIT->second = utilcounter;//Then we replace the old utility count value...
+
+			it->second.insert(pair<string, int> (ipaddr, utilcounter));// And insert this connection/util counter combination
+
+			//Sort the map (the map of utility count/connection IP address pairs for this connection) before returning:
+
+			vector<pair<string, int>> mapSort;
+
+			mapSort = new vector<pair<string, int>> (it->second.begin(), //In order to sort the interior map we need to convert it to a vector first
+					it->second.end());
+
+			sort(mapSort.begin(), mapSort.end(), &RoutingTable::Routing_SortConnections);//We can now sort the map using the "Routing_SortConnections" function (defined in this file)
+
+		} // otherwise we don't need to do anything (The new route was not fewer hops)
+
+	}
+
+	return 0;//Indicate success
 }
 
-bool Routing_SortConnections (pair<string, int> first, pair<string, int> second) {
+/**
+ * Function to remove an entry from the routing table.
+ *
+ * Takes:
+ * 		"rmAddr"
+ * 			A VirtualAddress (see RoutingTable.h) indicating the destination virtual address entry
+ * 			in the table to remove
+ *
+ * Returns an int indicating success (0) or failure (1)
+ */
+int RoutingTable::RemoveTableEntry(VirtualAddress rmAddr) {
+
+	it = table.find(rmAddr);// Find the entry to remove
+
+	//If we didn't find anything, return an error
+	if (it == table.end())
+		return 1;
+
+	table.erase(it);//Otherwise we just remove it
+
+	return 0;//Indicate success
+}
+
+/**
+ * Function used to sort the map of connection information for a given virtual address
+ * Takes:
+ * 		"first" and "second"
+ * 			The information for two connections to put in order
+ * Returns:
+ * 		True if "first" and "second" are in the correct order.
+ * 		Otherwise returns false.
+ */
+bool Routing_SortConnections(pair<string, int> first, pair<string, int> second) {
 	if (first.second <= second.second) {
 		return true;
 	} else {
 		return false;
 	}
 }
+
+RoutingTable::~RoutingTable(void) {
+//TODO: Put code here to store the routing table to a file before exit
+}
+

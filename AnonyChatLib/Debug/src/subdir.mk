5b4fa90aa1d8623553d94e150c68b7c6f3a14c09
PaulaRudy <paularudy@gmail.com>
2014-02-23 14:19:03 -0500

    Change to project files.

--- AnonyChatLib/Debug/src/subdir.mk
+++ AnonyChatLib/Debug/src/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -std=c++0x -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/home/arsenic/AnonyChat/AnonyChatLib/include" -I"/home/arsenic/AnonyChat/AnonyChatLib/include/ContactList" -I"/home/arsenic/AnonyChat/AnonyChatLib/include/Routing" -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++0x -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
